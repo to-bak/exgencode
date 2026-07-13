407c6011b7081f95e8b5b068895076db5111d7ca
to-bak <oliverbkp@gmail.com>
2026-07-13 15:14:31 +0200

    remove sloppy doc

--- lib/decode_error.ex
+++ lib/decode_error.ex
@@ -1,8 +1,4 @@
 defmodule Exgencode.DecodeError do
-  @moduledoc """
-  Raised during decoding when an `:offset_to` field points to an invalid
-  location - either backwards (before the current cursor, into already-parsed
-  bytes) or past the end of the binary.
-  """
+  @moduledoc false
   defexception [:message]
 end
